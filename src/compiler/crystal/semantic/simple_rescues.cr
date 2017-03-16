95624f879382b8eaa10b9b91ce4d388c970591cf
Brian J. Cardiff <bcardiff@manas.tech>
2017-03-16 12:49:24 -0300

    SimpleRescues: fix variable/method name

--- src/compiler/crystal/semantic/simple_rescues.cr
+++ src/compiler/crystal/semantic/simple_rescues.cr
@@ -12,39 +12,39 @@ module Crystal
       if rescues
         if rescues.size > 1
           var_name = next_var
-          ensure_body = Call.global("raise", Var.new(var_name))
+          rescue_body = Call.global("raise", Var.new(var_name))
 
           rescues.reverse_each do |rescue_node|
-            typed_var_name, restriction_type, body = split_ensure rescue_node
+            typed_var_name, restriction_type, body = split_rescue rescue_node
             if restriction_type
-              ensure_body = type_restricted_ensure_body(var_name,
-                typed_var_name, restriction_type, body.transform(self), ensure_body)
+              rescue_body = type_restricted_rescue_body(var_name,
+                typed_var_name, restriction_type, body.transform(self), rescue_body)
             else
-              ensure_body = Expressions.from [
+              rescue_body = Expressions.from [
                 Assign.new(Var.new(typed_var_name), Var.new(var_name)),
                 body,
               ]
             end
           end
         else
-          typed_var_name, restriction_type, body = split_ensure rescues.first
+          typed_var_name, restriction_type, body = split_rescue rescues.first
           var_name = typed_var_name
           if restriction_type
-            ensure_body = If.new(
+            rescue_body = If.new(
               IsA.new(Var.new(typed_var_name), restriction_type),
               body, Call.global("raise", Var.new(var_name)))
           else
-            ensure_body = body
+            rescue_body = body
           end
         end
 
-        node.rescues = [Rescue.new(ensure_body, nil, var_name)]
+        node.rescues = [Rescue.new(rescue_body, nil, var_name)]
       end
 
       node
     end
 
-    private def split_ensure(node : Rescue)
+    private def split_rescue(node : Rescue)
       types = node.types
 
       if types
@@ -56,7 +56,7 @@ module Crystal
       {node.name || next_var, restriction_type, node.body}
     end
 
-    private def type_restricted_ensure_body(var_name : String,
+    private def type_restricted_rescue_body(var_name : String,
                                             typed_var_name : String, restriction_type : ASTNode,
                                             body : ASTNode, cont : ASTNode)
       If.new(
