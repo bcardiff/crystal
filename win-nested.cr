e2b65509296223a24121ab80ed6179bc04b03d01
Brian J. Cardiff <bcardiff@manas.tech>
2017-03-16 15:20:41 -0300

    Standalone windows exception samples

--- win-nested.cr
+++ win-nested.cr
@@ -1,11 +1,5 @@
-# Visual C++ Build Tools
-# $ make clean crystal && DUMP=1 ./bin/crystal build win-raise.cr --cross-compile --target "x86_64-pc-windows-msvc19.0.0" --prelude=empty --ll
-# D:\> link win-raise.o -defaultlib:libcmt
-# D:\> win-raise.exe
-# ABCDEYZ
-
 lib LibC
-  fun putchar(c : UInt8)
+  fun putchar(c : UInt8) : Void
 end
 
 lib LibWindows
@@ -20,9 +14,7 @@ end
 
 @[Raises]
 fun __crystal_raise(ex : Void*) : NoReturn
-  LibC.putchar 67u8
   ti = WindowsExt.throw_info.as(Pointer({Int32, Int32, Int32, Int32}))
-  LibC.putchar 68u8
   LibWindows.cxx_throw_exception(ex, ti)
 end
 
@@ -42,7 +34,14 @@ end
 LibC.putchar 65u8
 begin
   LibC.putchar 66u8
-  raise Exception.new(69u8)
+  begin
+    LibC.putchar 67u8
+    raise Exception.new(33u8)
+  rescue e
+    LibC.putchar e.message
+    LibC.putchar 88u8
+    raise e
+  end
 rescue e
   LibC.putchar e.message
   LibC.putchar 89u8
