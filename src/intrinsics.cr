737f80e29f1aaa42124ff673f1806d375ed415bc
Brian J. Cardiff <bcardiff@gmail.com>
2019-01-15 15:25:59 -0300

    Refactor lib Intrinsics to module to allow mem primitives for llvm 7.0

--- src/intrinsics.cr
+++ src/intrinsics.cr
@@ -1,14 +1,5 @@
-lib Intrinsics
+lib LibIntrinsics
   fun debugtrap = "llvm.debugtrap"
-  {% if flag?(:x86_64) %}
-    fun memcpy = "llvm.memcpy.p0i8.p0i8.i64"(dest : Void*, src : Void*, len : UInt64, align : UInt32, is_volatile : Bool)
-    fun memmove = "llvm.memmove.p0i8.p0i8.i64"(dest : Void*, src : Void*, len : UInt64, align : UInt32, is_volatile : Bool)
-    fun memset = "llvm.memset.p0i8.i64"(dest : Void*, val : UInt8, len : UInt64, align : UInt32, is_volatile : Bool)
-  {% else %}
-    fun memcpy = "llvm.memcpy.p0i8.p0i8.i32"(dest : Void*, src : Void*, len : UInt32, align : UInt32, is_volatile : Bool)
-    fun memmove = "llvm.memmove.p0i8.p0i8.i32"(dest : Void*, src : Void*, len : UInt32, align : UInt32, is_volatile : Bool)
-    fun memset = "llvm.memset.p0i8.i32"(dest : Void*, val : UInt8, len : UInt32, align : UInt32, is_volatile : Bool)
-  {% end %}
   fun read_cycle_counter = "llvm.readcyclecounter" : UInt64
   fun bswap32 = "llvm.bswap.i32"(id : UInt32) : UInt32
 
@@ -20,6 +11,60 @@ lib Intrinsics
 
   fun va_start = "llvm.va_start"(ap : Void*)
   fun va_end = "llvm.va_end"(ap : Void*)
+end
+
+module Intrinsics
+  def self.debugtrap
+    LibIntrinsics.debugtrap
+  end
+
+  @[Primitive(:memcpy)]
+  def self.memcpy(dest : Void*, src : Void*, len, align : UInt32, is_volatile : Bool) : Void
+  end
+
+  @[Primitive(:memmove)]
+  def self.memmove(dest : Void*, src : Void*, len, align : UInt32, is_volatile : Bool) : Void
+  end
+
+  @[Primitive(:memset)]
+  def self.memset(dest : Void*, val : UInt8, len, align : UInt32, is_volatile : Bool) : Void
+  end
+
+  def self.read_cycle_counter
+    LibIntrinsics.read_cycle_counter
+  end
+
+  def self.bswap32(id)
+    LibIntrinsics.bswap32(id)
+  end
+
+  def self.popcount8(src)
+    LibIntrinsics.popcount8(src)
+  end
+
+  def self.popcount16(src)
+    LibIntrinsics.popcount16(src)
+  end
+
+  def self.popcount32(src)
+    LibIntrinsics.popcount32(src)
+  end
+
+  def self.popcount64(src)
+    LibIntrinsics.popcount64(src)
+  end
+
+  def self.popcount128(src)
+    LibIntrinsics.popcount128(src)
+  end
+
+  def self.va_start(ap)
+    LibIntrinsics.va_start(ap)
+  end
+
+  def self.va_end(ap)
+    LibIntrinsics.va_end(ap)
+  end
 end
 
 macro debugger
