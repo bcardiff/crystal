86c13ee6ba54fea19a1bdfb05e4c53555f5edd44
Brian J. Cardiff <bcardiff@gmail.com>
2019-10-04 16:09:19 -0300

    Profiling: add fiber stats

--- src/crystal/profiling.cr
+++ src/crystal/profiling.cr
@@ -14,7 +14,11 @@ module Crystal::Profiling
 
     Thread.new do
       while !@@stop
-        emit_gc_prof_stats(profiling_file, nil, Time.local.to_unix)
+        timestamp = Time.local.to_unix
+
+        emit_gc_prof_stats(profiling_file, nil, timestamp)
+        emit_fibers_stats(profiling_file, nil, timestamp)
+
         sleep interval
       end
 
@@ -47,6 +51,26 @@ module Crystal::Profiling
     emit_metric_value io, "gc_prof_stats_reclaimed_bytes_before_gc", s.reclaimed_bytes_before_gc, attributes, timestamp
   end
 
+  def self.emit_fibers_stats(io, attributes = nil, timestamp = nil)
+    running = 0
+    resumable = 0
+    dead = 0
+
+    Fiber.unsafe_each do |f|
+      if f.dead?
+        dead += 1
+      elsif f.running?
+        running += 1
+      elsif f.resumable?
+        resumable += 1
+      end
+    end
+
+    emit_metric_value io, "fibers", running, merge_attributes(attributes, {state: "running"}), timestamp
+    emit_metric_value io, "fibers", resumable, merge_attributes(attributes, {state: "resumable"}), timestamp
+    emit_metric_value io, "fibers", dead, merge_attributes(attributes, {state: "dead"}), timestamp
+  end
+
   def self.emit_metric_value(io, name, value, attributes, timestamp)
     io << name
     emit_metric_attibutes(io, attributes) if attributes
@@ -73,5 +97,13 @@ module Crystal::Profiling
       end
     end
     io << "}"
+  end
+
+  def self.merge_attributes(attr1, attr2)
+    if attr1 && attr2
+      attr1.merge(attr2)
+    else
+      attr1 || attr2
+    end
   end
 end
